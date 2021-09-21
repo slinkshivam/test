b602e57eb81c7e104f2c671d12033eb52772e316
slinkshivam <47948198+slinkshivam@users.noreply.github.com>
2021-09-21 16:10:09 +0530

    Add files via upload

---
+++ 006-agency-minimal-gatsby/src/pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'swiper/swiper-bundle.min.css';
+import 'rc-drawer/assets/index.css';
+import 'react-modal-video/css/modal-video.min.css';
+
+import { StickyProvider } from 'contexts/app/app.provider';
+import SEO from 'components/seo';
+import Layout from 'components/layout';
+
+import Banner from 'sections/banner';
+import Services from 'sections/services';
+import Testimonials from 'sections/testimonials';
+import CustomerSupport from 'sections/customer-support';
+import Feature from 'sections/feature';
+import VideoOne from 'sections/video-one';
+import CallToAction from 'sections/call-to-action';
+import BoostAgencies from 'sections/boost-agencies';
+
+export default function IndexPage() {
+  return (
+    <StickyProvider>
+      <Layout>
+        <SEO title="Startup Landing 006" />
+        <Banner />
+        <Services />
+        <BoostAgencies />
+        <VideoOne />
+        <Testimonials />
+        <CustomerSupport />
+        <Feature />
+        <CallToAction />
+      </Layout>
+    </StickyProvider>
+  );
+}
